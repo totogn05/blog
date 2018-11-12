a7b5706c4d94597aca8631f580c4036cb962fdde
user <user@MW-062>
2018-11-12 12:45:25 +0800

    first push

--- blog/main/views.py
+++ blog/main/views.py
@@ -4,6 +4,18 @@ from django.http import HttpResponse
 
 def main(request):
     '''
-    Show 'Hello world!' in the main page
+    Render the main page
     '''
-    return HttpResponse('Hello world!')
+    html = '''
+    <!doctype html>
+    <html>
+    <head>
+    <title>部落格</title>
+    <meta charset="utf-8">
+    </head>
+    <body>
+    <p>這是 HTML 版的 Hello world!</p>
+    </body>
+    </html>
+    '''
+    return HttpResponse(html)
